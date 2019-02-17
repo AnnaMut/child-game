6b693ec70493bb6c968f26b3ce27bdfd46965dfe
AnnaMut <butorovaanna@yandex.ru>
2019-02-18 00:38:19 +0300

    исправлены повторы картинок

--- js/game-data.js
+++ js/game-data.js
@@ -1,18 +1,30 @@
 export const CARDS_COUNT = 18;
 
-const getRandomFromInterval = function (min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-};
+// const getRandomFromInterval = function (min, max) {
+// return Math.floor(Math.random() * (max - min) + min);
+// };
 
-const getImgScr = ()=> {
-  const src = [];
-  for (let i = 0; i <= 8; i++) {
-    src.push(getRandomFromInterval(1, 19));
+const getRandomSet = (min, max, number) => {
+  let res = new Set();
+  while (res.size < number) {
+    res.add(Math.floor(Math.random() * (max - min + 1)) + min);
   }
-  return src;
+  return res;
 };
 
-const cardsNumber = getImgScr();
+const setImg = getRandomSet(1, 19, 9);
+
+// const getImgScr = ()=> {
+// const src = [];
+// for (let i = 0; i <= 8; i++) {
+//   src.push(getRandomFromInterval(1, 19));
+// }
+// return src;
+// };
+
+// const cardsNumber = getImgScr();
+const cardsNumber = [...setImg];
+
 export const allcardsNumber = [...cardsNumber, ...cardsNumber];
 
 const getCardsSrc = ()=> {
